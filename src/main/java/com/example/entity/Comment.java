751f72580e6dbdbabddd320a856a7de61f5d10bf
Quolen <oleg.silvika@gmail.com>
2024-04-18 21:39:31 +0200

    Implemented Comment model.

--- src/main/java/com/example/entity/Comment.java
+++ src/main/java/com/example/entity/Comment.java
@@ -1,7 +1,35 @@
 package com.example.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Data
 @Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Post post;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(columnDefinition = "text", nullable = false)
+    private String message;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
