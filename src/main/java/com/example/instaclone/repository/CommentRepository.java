994130ca7b9b91a324f5f3d9a368ca8cc9768e1f
Quolen <oleg.silvika@gmail.com>
2024-04-19 14:47:39 +0200

    Implemented the necessary Repositories.

---
+++ src/main/java/com/example/instaclone/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.instaclone.repository;
+
+import com.example.instaclone.entity.Comment;
+import com.example.instaclone.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(Long commentId, Long userId);
+}
