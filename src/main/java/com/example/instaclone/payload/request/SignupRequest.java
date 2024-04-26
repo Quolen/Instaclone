acf6fc99d86c5128f7f0c7851a8411c6fb6521f4
Quolen <oleg.silvika@gmail.com>
2024-04-26 19:06:46 +0200

    Implemented LoginRequest for capturing the login data. Implemented SignupRequest with custom @ValidEmail annotation for capturing the data to sign up the user.

---
+++ src/main/java/com/example/instaclone/payload/request/SignupRequest.java
@@ -0,0 +1,31 @@
+package com.example.instaclone.payload.request;
+
+import com.example.instaclone.annotations.ValidEmail;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "User email is required")
+    @ValidEmail
+    private String email;
+
+    @NotEmpty(message = "Please enter your name")
+    private String firstname;
+
+    @NotEmpty(message = "Please enter your lastname")
+    private String lastname;
+
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+    private String confirmPassword;
+}
