18b22a752be15f6a8534adafbdd476aac34bed36
Quolen <oleg.silvika@gmail.com>
2024-04-28 20:41:14 +0200

    Finished implementing UserService class. Created UserDTO class that will help us transfer user information to the clients.

--- src/main/java/com/example/instaclone/services/UserService.java
+++ src/main/java/com/example/instaclone/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.instaclone.services;
 
 
+import com.example.instaclone.dto.UserDTO;
 import com.example.instaclone.entity.User;
 import com.example.instaclone.entity.enums.ERole;
 import com.example.instaclone.exceptions.UserExistException;
@@ -8,15 +9,16 @@ import com.example.instaclone.payload.request.SignupRequest;
 import com.example.instaclone.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
+
 
 /*
- Service for creating new users.
-
- Receives SignupRequest object with necessary User credentials
- and saves User to the database.*/
+ User Service for managing user related logic.
+ */
 @Service
 public class UserService {
 
@@ -46,5 +48,24 @@ public class UserService {
             LOG.error("Error during registration. {}", e.getMessage());
             throw new UserExistException("The user with such email \"" + user.getEmail() + "\" already exists. Please check your credentials.");
         }
+    }
+
+    public User updateUser(UserDTO userDTO, Principal principal) {
+        User user = getUserByPrincipal(principal);
+        user.setName(userDTO.getFirstname());
+        user.setLastname(userDTO.getLastname());
+        user.setBio(userDTO.getBio());
+
+        return userRepository.save(user);
+    }
+
+    public User getCurrentUser(Principal principal) {
+        return getUserByPrincipal(principal);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not fount"));
     }
 }
