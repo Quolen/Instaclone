4b2ebbb89ffa62e1a3425d6b1d121acf82fd911c
Quolen <oleg.silvika@gmail.com>
2024-04-29 10:55:43 +0200

    Implemented PostFacade class.

---
+++ src/main/java/com/example/instaclone/facade/PostFacade.java
@@ -0,0 +1,21 @@
+package com.example.instaclone.facade;
+
+import com.example.instaclone.dto.PostDTO;
+import com.example.instaclone.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostFacade {
+
+    public PostDTO postToPostDTO(Post post) {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setId(post.getId());
+        postDTO.setUsername(post.getUser().getUsername());
+        postDTO.setCaption(post.getCaption());
+        postDTO.setLikes(post.getLikes());
+        postDTO.setUsersLiked(post.getLikedUsers());
+        postDTO.setLocation(post.getLocation());
+        postDTO.setTitle(post.getTitle());
+        return postDTO;
+    }
+}
