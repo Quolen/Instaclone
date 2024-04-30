10aca2fe6fc3e52114b926bc1b2b3abc5e2a2c64
Quolen <oleg.silvika@gmail.com>
2024-04-30 21:51:20 +0200

    Fixed a typo.

--- src/main/java/com/example/instaclone/security/JWTTokenProvider.java
+++ src/main/java/com/example/instaclone/security/JWTTokenProvider.java
@@ -57,7 +57,7 @@ public class JWTTokenProvider {
     public Long getUserIdFromToken(String token) {
         Claims claims = Jwts.parser()
                 .setSigningKey(SecurityConstants.SECRET)
-                .parseClaimsJwt(token)
+                .parseClaimsJws(token)
                 .getBody();
         String id  = (String) claims.get("id");
         return Long.parseLong(id);
