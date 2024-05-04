c942234345e27b912ca2d8694caeb561e381ed53
Quolen <oleg.silvika@gmail.com>
2024-05-04 15:32:53 +0200

    Implemented token-storage.service.ts class which is responsible for saving and getting the Token and User.

--- instaclone_front/src/app/service/token-storage.service.ts
+++ instaclone_front/src/app/service/token-storage.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenStorageService {
+  constructor() {}  // No setup required
 
-  constructor() { }
+  public saveToken(token: string): void {
+    sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return sessionStorage.getItem(TOKEN_KEY) ?? '';
+  }
+
+  public saveUser(user: any): void {
+    sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    const user = sessionStorage.getItem(USER_KEY);
+    return user ? JSON.parse(user) : null;
+  }
+
+  public logOut(): void {
+    sessionStorage.clear();
+    window.location.reload();
+  }
 }
