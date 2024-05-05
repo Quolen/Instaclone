4a0850d49cccf79e546ac3e770350e010a8d51cb
Quolen <oleg.silvika@gmail.com>
2024-05-05 08:59:41 +0200

    Implemented auth-guard.service.ts for checking whether the user has access to some route.

--- instaclone_front/src/app/helper/auth-guard.service.ts
+++ instaclone_front/src/app/helper/auth-guard.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CanMatchFn, Route, UrlSegment, Router } from '@angular/router';
+import { TokenStorageService } from '../service/token-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuardService {
+  constructor(private router: Router, private tokenService: TokenStorageService) {}
 
-  constructor() { }
+  canMatch: CanMatchFn = (route: Route, segments: UrlSegment[]): boolean => {
+    const currentUser = this.tokenService.getUser();
+    if (currentUser) {
+      return true;  // User has access
+    }
+
+    this.router.navigate(['/login']);  // Redirect if not logged in
+    return false;  // Block the route
+  };
 }
