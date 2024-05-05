217f4648ff62084043d630e44bafc57ce6b905fc
Quolen <oleg.silvika@gmail.com>
2024-05-05 11:32:52 +0200

    Implemented login component which is responsible for login UI.

--- instaclone_front/src/app/auth/login/login.component.ts
+++ instaclone_front/src/app/auth/login/login.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../service/auth.service";
+import {TokenStorageService} from "../../service/token-storage.service";
+import {NotificationService} from "../../service/notification.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  public loginForm!: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private notificationService: NotificationService,
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+    if (this.tokenStorage.getUser()) {
+      this.router.navigate(['main']);
+    }
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.createLoginForm();
+  }
+
+  createLoginForm(): FormGroup {
+    return this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  submit(): void {
+    this.authService
+      .login({
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password,
+      })
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+
+          this.tokenStorage.saveToken(data.token);
+          this.tokenStorage.saveUser(data);
+
+          this.notificationService.showSnackBar('Successfully logged in');
+          this.router.navigate(['/']);
+          window.location.reload();
+        },
+        error: (error) => {
+          console.log(error);
+          this.notificationService.showSnackBar('Login error');
+        },
+      });
+  }
 }
