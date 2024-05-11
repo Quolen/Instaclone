45f702a2ebf7605c758d1c9b76448e1caf26bdbd
Quolen <oleg.silvika@gmail.com>
2024-05-11 10:57:30 +0200

    Fixed invalid credentials handling while logging in.

--- instaclone_front/src/app/helper/error-interceptor.service.ts
+++ instaclone_front/src/app/helper/error-interceptor.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {TokenStorageService} from "../service/token-storage.service";
 import {NotificationService} from "../service/notification.service";
 import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
@@ -7,21 +7,21 @@ import {catchError, Observable, throwError} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorInterceptorService implements HttpInterceptor {
+export class ErrorInterceptorService implements HttpInterceptor{
 
   constructor(private tokenService: TokenStorageService,
-              private notificationService: NotificationService) { }
+              private notificationService: NotificationService) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(catchError(err => {
       if (err.status === 401) {
-        this.tokenService.logOut();
-        window.location.reload();
+        this.notificationService.showSnackBar('Login error');
+        setTimeout(() => {
+          this.tokenService.logOut();
+        }, 1000);
       }
-
-      const error = err.error.message || err.statusText;
-      this.notificationService.showSnackBar(error);
-      return throwError(error);
+      return throwError(err);
     }));
   }
 }
