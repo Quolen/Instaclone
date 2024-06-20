5ff8e9730b05fafaac54663a50bc41b726378f50
Quolen <oleg.silvika@gmail.com>
2024-06-20 09:59:34 +0200

    Did some small fixes.

--- instaclone_back/src/test/java/com/example/instaclone/InstacloneApplicationTests.java
+++ instaclone_back/src/test/java/com/example/instaclone/InstacloneApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.instaclone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = InstacloneApplication.class)
 class InstacloneApplicationTests {
 
     @Test
